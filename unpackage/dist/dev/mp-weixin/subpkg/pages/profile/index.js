f247bd4d026600eef41b33cc08197e34e2c67f73
xiaoqu <1047910806@qq.com>
2022-06-28 20:35:09 +0800

    feat：增加功能

---
+++ unpackage/dist/dev/mp-weixin/subpkg/pages/profile/index.js
@@ -0,0 +1,31 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["subpkg/pages/profile/index"],{
+
+/***/ 64:
+/*!*******************************************************************************************************!*\
+  !*** D:/My Dom/前端/进阶学习资料/小程序/HbuilderX/zuoyebang/main.js?{"page":"subpkg%2Fpages%2Fprofile%2Findex"} ***!
+  \*******************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 4));
+var _index = _interopRequireDefault(__webpack_require__(/*! ./subpkg/pages/profile/index.vue */ 65));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+createPage(_index.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 65:
+/*!**********************************************************************************!*\
+  !*** D:/My Dom/前端/进阶学习资料/小程序/HbuilderX/zuoyebang/subpkg/pages/profile/index.vue ***!
+  \**********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'D:\\My Dom\\前端\\进阶学习资料\\小程序\\HbuilderX\\zuoyebang\\subpkg\\pages\\profile\\index.vue'");
+
+/***/ })
+
+},[[64,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../../.sourcemap/mp-weixin/subpkg/pages/profile/index.js.map
